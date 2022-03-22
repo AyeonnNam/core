7487f37471acb1ed25f7a224617fc10bea4e4441
name <boalove284@naver.com>
2022-03-22 14:46:36 +0900

    pure java code -> spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-//순수 자바 코드에서 스프링으로 전환하기
+//순수 자바 코드에서 스프링으로 전환하기!!
 @Configuration
 public class AppConfig {
 
