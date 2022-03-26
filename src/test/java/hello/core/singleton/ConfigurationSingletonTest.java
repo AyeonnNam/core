9cf6c2d3850311936ef9d93d2bf7fe417d7959e3
name <boalove284@naver.com>
2022-03-26 14:14:50 +0900

    SingletonServiceTest

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -44,10 +44,10 @@ public class ConfigurationSingletonTest {
 
         //bean = hello.core.AppConfig$$EnhancerBySpringCGLIB$$ee4ff54b@52d645b1
         /* 클래스명 이외에도 이상한게 있음
-        * hello.core.AppConfig = 순수한 클래스 명
-        * bean = hello.core.AppConfig$$EnhancerBySpringCGLIB$$ee4ff54b@52d645b1
-        * 다른걸 빈으로 등록 (내가 만든게 아니라 바이트코드를 스프링이 조작해 appconfig를 상속받은 임의의 클래스를 등록한 것.)
-        * 내가 만든 건 사라지고 appconfig란 이름으로 다른 임의의 개체가 싱글톤이 될 수 있도록 보장해주는 것.
-        * */
+         * hello.core.AppConfig = 순수한 클래스 명
+         * bean = hello.core.AppConfig$$EnhancerBySpringCGLIB$$ee4ff54b@52d645b1
+         * 다른걸 빈으로 등록 (내가 만든게 아니라 바이트코드를 스프링이 조작해 appconfig를 상속받은 임의의 클래스를 등록한 것.)
+         * 내가 만든 건 사라지고 appconfig란 이름으로 다른 임의의 개체가 싱글톤이 될 수 있도록 보장해주는 것.
+         * */
     }
 }
