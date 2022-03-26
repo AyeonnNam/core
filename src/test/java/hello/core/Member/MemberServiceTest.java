3b0742d744dbfff20d660fc3a3258b1040e66d45
name <boalove284@naver.com>
2022-03-26 14:52:52 +0900

    MemberService memberService;     OrderService orderService;
                AppConfig appConfig  = new AppConfig();
                 orderService = appConfig.orderService();
                 memberService = appConfig.memberService();

--- src/test/java/hello/core/Member/MemberServiceTest.java
+++ src/test/java/hello/core/Member/MemberServiceTest.java
@@ -1,7 +1,11 @@
 package hello.core.Member;
 
 
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -9,14 +13,24 @@ import static org.assertj.core.api.Assertions.*;
 public class MemberServiceTest {
 
     MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+
+    }
     @Test
     void join(){
+
+
         //회원가입. 회원조회
 
         //given
         Member member = new Member(1L, Grade.VIP,"남아연");
 
         //when
+
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
         //then
--- src/test/java/hello/core/Order/OrderServiceTest.java
+++ src/test/java/hello/core/Order/OrderServiceTest.java
@@ -20,8 +20,8 @@ public class OrderServiceTest {
     @BeforeEach
     public void beforeEach(){
         AppConfig appConfig  = new AppConfig();
-        OrderService orderService = appConfig.orderService();
-        MemberService memberService = appConfig.memberService();
+         orderService = appConfig.orderService();
+         memberService = appConfig.memberService();
     }
    // MemberService memberService = new MemberServiceImpl(memberRepository);
    // OrderService orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
