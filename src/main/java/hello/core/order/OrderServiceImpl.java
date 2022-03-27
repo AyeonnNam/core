2b10ad73b9008f66b5b0080fe1080649238e23aa
name <boalove284@naver.com>
2022-03-27 12:08:43 +0900

    Add, use lombok library, constructor omission

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.Member.Member;
 import hello.core.Member.MemberRepository;
 import hello.core.discount.DiscountPolicy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
 
@@ -18,11 +20,11 @@ public class OrderServiceImpl implements OrderService{
 
 
 
-    @Autowired //생성자가 하나일때는 Autowired 어노테이션없이 spring container가 자동으로 의존관계 등록함.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired //생성자가 하나일때는 Autowired 어노테이션없이 spring container가 자동으로 의존관계 등록함.
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     @Override
